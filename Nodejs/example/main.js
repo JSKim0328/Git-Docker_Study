18e616dcfc9cc74dec9cd22c6438a38bf1bf7daa
JSKim0328 <jungsik328@naver.com>
2019-09-04 00:15:56 +0900

    resolve err by using callback

--- Nodejs/example/main.js
+++ Nodejs/example/main.js
@@ -19,7 +19,7 @@ var app = http.createServer(function (request, response) {
   } else {
     title = queryData.id;
   }
-  
+
   fs.readFile(`data/${title}`, 'utf-8', function (err, description) {
 
     var list = '';
@@ -29,35 +29,35 @@ var app = http.createServer(function (request, response) {
       list += '<ol>';
 
       for (var i = 0; i < flist.length; ++i) {
+        if (flist[i] === 'Welcome') continue;
         list += `<li><a href="/?id=${flist[i]}">${flist[i]}</a></li>`;
       }
 
       list += '</ol>';
-      console.log(list);
-    });
 
-    // 이렇게 하면 자바스크립트의 특징인 비동기 처리 때문에 list가 undefined라고 뜸.
-
-    var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-        <p>
-        ${description}
-        </p>
-      </body>
-      </html>
+      var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>
+          ${description}
+          </p>
+        </body>
+        </html>
       `;
 
-    response.writeHead(200);
-    response.end(template);
+      response.writeHead(200);
+      response.end(template);
+
+    });
+
   });
 
 
