8f053155e42382354f28770e63053bb818be8429
JSKim0328 <jungsik328@naver.com>
2019-09-16 14:40:46 +0900

    express hello world

--- Nodejs/example/main.js
+++ Nodejs/example/main.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/another', function (req, res) {
+  res.send('Hello Another!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
